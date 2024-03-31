a27787e5dd500bdf27b335e97f0a8d4fa52bde40
Cristian I. Vargas <civargas34@gmail.com>
2024-03-31 00:40:45 -0400

    Clean up controller

--- projects/OMSATrackingAPI/OMSATrackingAPI/Controllers/RouteController.cs
+++ projects/OMSATrackingAPI/OMSATrackingAPI/Controllers/RouteController.cs
@@ -14,26 +14,11 @@ namespace OMSATrackingAPI.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetAll()
         {
             var response = await _service.GetAll();
-            if (response.IsSuccess)
-            {
-                return Ok(response);
-            }
-
-            return BadRequest(response);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Insert([FromBody] DAL.Models.Route route)
-        {
-            if (route == null)
-            {
-                return BadRequest("Invalid data");
-            }
-
-            var response = await _service.Insert(route);
             if (response.IsSuccess)
             {
                 return Ok(response);
