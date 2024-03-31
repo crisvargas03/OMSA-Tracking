5c625bdae79ab2c44d931314b197a9faed35abfe
Cristian I. Vargas <civargas34@gmail.com>
2024-03-31 00:25:11 -0400

    Add Included properties options

--- projects/OMSATrackingAPI/OMSATrackingAPI.DAL/Repository/Core/IBaseRepository.cs
+++ projects/OMSATrackingAPI/OMSATrackingAPI.DAL/Repository/Core/IBaseRepository.cs
@@ -5,7 +5,7 @@ namespace OMSATrackingAPI.DAL.Repository.Core
     public interface IBaseRepository<T> where T : class
     {
         Task<T> AddAsync(T entity);
-        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
-        Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true, params Expression<Func<T, object>>[] includes);
+        Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true, params Expression<Func<T, object>>[] includes);
     }
 }
