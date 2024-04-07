a134a3d17c259745c89e81e47e3f8f4ce143a32e
Naeliza <lizcespedes0326@gmail.com>
2024-04-07 12:14:38 -0400

    Optimizando Update para los buses

--- projects/OMSATrackingAPI/OMSATrackingAPI.DAL/Repository/BusRepository.cs
+++ projects/OMSATrackingAPI/OMSATrackingAPI.DAL/Repository/BusRepository.cs
@@ -15,77 +15,34 @@ namespace OMSATrackingAPI.DAL.Repository
             _context = context;
         }
 
+        #region Obtener ruta existente
         public async Task<bool> RouteExistsAsync(Expression<Func<Route, bool>> filter)
         {
             return await _context.Set<Route>().AnyAsync(filter);
         }
+        #endregion
 
-        public async Task<Bus> GetAsync(int id, bool tracked = true, params Expression<Func<Bus, object>>[] includes)
+        #region Obtener bus por id
+        public async Task<Bus> GetByIdAsync(int id)
         {
-            IQueryable<Bus> query = _context.Set<Bus>();
-
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-
-            foreach (var include in includes)
-            {
-                query = query.Include(include);
-            }
-
-            return await query.FirstOrDefaultAsync(x => x.Id == id);
+            return await _context.Set<Bus>().FindAsync(id);
         }
+        #endregion
 
-        public async Task<bool> UpdateBusAsync(int id, Bus updatedBus)
-        {
-            try
-            {
-                // Obtener el autobús existente por su ID
-                var existingBus = await _context.Set<Bus>().FindAsync(id);
-
-                // Verificar si el autobús existe
-                if (existingBus == null)
-                {
-                    return false; 
-                }
-
-                // Actualizar las propiedades del autobús existente con los valores del autobús actualizado
-                existingBus.Name = updatedBus.Name;
-                existingBus.Latitude = updatedBus.Latitude;
-                existingBus.Longitude = updatedBus.Longitude;
-                existingBus.Plate = updatedBus.Plate;
-                existingBus.EstimatedArrivalHour = updatedBus.EstimatedArrivalHour;
-                existingBus.PassengerLimit = updatedBus.PassengerLimit;
-                existingBus.RouteId = updatedBus.RouteId;
-
-                // Marcar el estado del autobús como modificado
-                _context.Entry(existingBus).State = EntityState.Modified;
-
-                // Guardar los cambios en la base de datos
-                await _context.SaveChangesAsync();
-
-                return true; 
-            }
-            catch (Exception)
-            {
-                return false; 
-            }
-        }
-
-
+        #region Eliminar
         public async Task<bool> DeleteBusAsync(int id)
         {
             var busToDelete = await _context.Set<Bus>().FindAsync(id);
             if (busToDelete != null)
             {
-                busToDelete.IsDeleted = true; 
+                busToDelete.IsDeleted = true;
                 _context.Entry(busToDelete).State = EntityState.Modified;
                 await SaveAsync();
                 return true;
             }
             return false;
         }
+        #endregion
 
     }
 }
